97629ecb26fa8231455d9c9478504318de261991
Lotshot <g3957177@gmail.com>
2025-08-04 04:32:15 +0400

    Configure testnet NFT collection

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,8 +1,11 @@
 export const SWAP_CONTRACT = import.meta.env.VITE_SWAP_CONTRACT ?? '';
-export const NFT_CONTRACT = import.meta.env.VITE_NFT_CONTRACT ?? '';
+export const NFT_CONTRACT =
+  import.meta.env.VITE_NFT_CONTRACT ??
+  'kQBkKUborBX1GPlhB0fLUoLFMeKYpe0DAAvWuu4sJnaWnd3b';
 export const TONCENTER_URL =
-  import.meta.env.VITE_TONCENTER_URL ?? 'https://toncenter.com/api/v2/jsonRPC';
+  import.meta.env.VITE_TONCENTER_URL ??
+  'https://testnet.toncenter.com/api/v2/jsonRPC';
 export const TON_API_URL =
-  import.meta.env.VITE_TON_API_URL ?? 'https://tonapi.io/v2';
+  import.meta.env.VITE_TON_API_URL ?? 'https://testnet.tonapi.io/v2';
 export const SITE_DOMAIN =
   import.meta.env.VITE_SITE_DOMAIN ?? 'nft2ton.onrender.com';
